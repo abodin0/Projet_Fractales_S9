2669b3296831a5b77441f6d117fbc23c56c3bdc7
abodin0 <antton.bodin@live.com>
2020-12-18 13:07:48 +0100

    passage en 2D de la config GPU

--- src/Convergence/double/Calcul_GPU.cu
+++ src/Convergence/double/Calcul_GPU.cu
@@ -3,49 +3,85 @@
 
 __global__ void kernel_updateImage_GPU(const long double _zoom, const long double _offsetX, const long double _offsetY, const int IMAGE_WIDTH, const int IMAGE_HEIGHT, uint32_t * deviceTab, int max_iters)
 {
-    int i = blockIdx.x * blockDim.x + threadIdx.x;
-    int nb_point = IMAGE_WIDTH * IMAGE_HEIGHT;
-    int x, y = 0;
+    int blockID = blockIdx.x * blockIdx.y + blockIdx.x;
+    int threadID = blockID * (blockDim.x * blockDim.y) + (threadIdx.y * blockDim.x) + threadIdx.x;
+
+    //printf("blockID : %d, threadID : %d\n", blockID, threadID);
+
+    //const int nb_point = IMAGE_WIDTH * IMAGE_HEIGHT;
 
     double offsetX = _offsetX;
     double offsetY = _offsetX;
     double zoom    = _zoom;
 
-    if(i<nb_point)
-    {
-      /*
-      x = i%IMAGE_WIDTH;
-      y = (i-x)/IMAGE_WIDTH;
+    int x = threadID%IMAGE_WIDTH;
+    int y = threadID/IMAGE_WIDTH;
 
-      double startImag = offsetY - IMAGE_HEIGHT / 2.0f * zoom + (y * zoom);
-      double startReal = offsetX - IMAGE_WIDTH  / 2.0f * zoom;
-      int value    = max_iters - 1;
-      double zReal = startReal;
-      double zImag = startImag;
+    //printf("x = %d, y = %d\n", x,y);
 
-      for (unsigned int counter = 0; counter < max_iters; counter++) 
-      {
-          double r2 = zReal * zReal;
-          double i2 = zImag * zImag;
-          zImag = 2.0f * zReal * zImag + startImag;
-          zReal = r2 - i2 + startReal;
-          if ( (r2 + i2) > 4.0f) {
-              value = counter;
-              break;
-          }
-      }
-
-      deviceTab[x+y*IMAGE_WIDTH] = value;
-      startReal += zoom;
-      */
-     deviceTab[x+y*IMAGE_WIDTH] = i%256;
-    }
-    //printf("value (GPU) = %x \n", deviceTab[50+200*IMAGE_WIDTH]);
-}
-
+    double startImag = offsetY - IMAGE_HEIGHT / 2.0f * zoom + (y * zoom);
+    double startReal = offsetX - IMAGE_WIDTH  / 2.0f * zoom;
 /*
-void Calcul_GPU::updateImage_GPU(int nblocks, int nthreads, const long double _zoom, const long double _offsetX, const long double _offsetY, const int IMAGE_WIDTH, const int IMAGE_HEIGHT, uint32_t * deviceTab, int max_iters)
-{
-  kernel_updateImage_GPU<<<nblocks, nthreads>>>(_zoom, _offsetX, _offsetY, IMAGE_WIDTH, IMAGE_HEIGHT, deviceTab, max_iters);
+   //for (int x = 0; x < IMAGE_WIDTH; x++)
+    //{
+        int value    = max_iters - 1;
+        double zReal = startReal;
+        double zImag = startImag;
+
+        for (unsigned int counter = 0; counter < max_iters; counter++) 
+        {
+            double r2 = zReal * zReal;
+            double i2 = zImag * zImag;
+            zImag = 2.0f * zReal * zImag + startImag;
+            zReal = r2 - i2 + startReal;
+            if ( (r2 + i2) > 4.0f) {
+                value = counter;
+                break;
+            }
+        }
+        __syncthreads();
+       // deviceTab[y*IMAGE_WIDTH+x] = value;
+        //startReal += zoom;
+    //}
+    */
+    deviceTab[y*IMAGE_WIDTH+x] = threadID%256;
 }
-*/
+
+
+__global__ void kernel_1D_updateImage_GPU(const long double _zoom, const long double _offsetX, const long double _offsetY, const int IMAGE_WIDTH, const int IMAGE_HEIGHT, uint32_t * deviceTab, int max_iters)
+{
+    int threadID = blockIdx.x * blockDim.x + threadIdx.x;;
+
+    //const int nb_point = IMAGE_WIDTH * IMAGE_HEIGHT;
+
+    double offsetX = _offsetX;
+    double offsetY = _offsetX;
+    double zoom    = _zoom;
+
+    //const int x = threadID%IMAGE_WIDTH;
+    int y = threadID/IMAGE_WIDTH;
+
+    double startImag = offsetY - IMAGE_HEIGHT / 2.0f * zoom + (y * zoom);
+    double startReal = offsetX - IMAGE_WIDTH  / 2.0f * zoom;
+
+    for (int x = 0; x < IMAGE_WIDTH; x++)
+    {
+        int value    = max_iters - 1;
+        double zReal = startReal;
+        double zImag = startImag;
+
+        for (unsigned int counter = 0; counter < max_iters; counter++) 
+        {
+            double r2 = zReal * zReal;
+            double i2 = zImag * zImag;
+            zImag = 2.0f * zReal * zImag + startImag;
+            zReal = r2 - i2 + startReal;
+            if ( (r2 + i2) > 4.0f) {
+                value = counter;
+                break;
+            }
+        }
+        deviceTab[y*IMAGE_WIDTH+x] = value;
+        startReal += zoom;
+    }
+}
--- src/Convergence/double/Convergence_GPU.cu
+++ src/Convergence/double/Convergence_GPU.cu
@@ -36,6 +36,13 @@ Convergence_GPU::Convergence_GPU(ColorMap* _colors, int _max_iters) : Convergenc
 
     hostTab = nullptr;
     deviceTab = nullptr;
+
+    cudaError_t cudaStatus;
+    cudaStatus = cudaSetDevice(0);
+    if ( cudaStatus != cudaSuccess ) {
+		printf( "error: unable to setup cuda device\n");
+        exit(0);
+	}
 }
 
 
@@ -47,29 +54,30 @@ Convergence_GPU::~Convergence_GPU( ){
 
 void Convergence_GPU::updateImage(const long double _zoom, const long double _offsetX, const long double _offsetY, const int IMAGE_WIDTH, const int IMAGE_HEIGHT, sf::Image& image)
 {
+    printf("Start GPU convergence\n");
     int nb_point = IMAGE_WIDTH*IMAGE_HEIGHT;
-    int nthreads = 1024;
-    int nblocks = ( nb_point + ( nthreads - 1 ) ) / nthreads;
+    dim3 grid(75,25,1); //nbr bloc
+    dim3 block(16,16,1); //nbr threads
 
-    cudaError_t cudaStatus;
-    cudaStatus = cudaSetDevice(0);
-    if ( cudaStatus != cudaSuccess ) {
-		printf( "error: unable to setup cuda device\n");
-	}
+/*
+    int nDevices;
+    cudaGetDeviceCount(&nDevices);
+    printf("Number of GPU : %d\n", nDevices);
+    for (int i = 0; i < nDevices; i++) {
+        cudaDeviceProp prop;
+        cudaGetDeviceProperties(&prop, i);
+        printf("Max grid size : %d\n", prop.maxGridSize);
+        printf("Max 2D surface : %d\n", prop.maxSurface2D);
+    }
+*/
 
     if(hostTab == nullptr)
         hostTab = new uint32_t[nb_point];
 
-    for(int i = 0 ; i<nb_point; i++){
-        hostTab[i] = i%256;
-    }
-
-    if(deviceTab == nullptr){
+    if(deviceTab == nullptr)
         CUDA_MALLOC((void**)&deviceTab, nb_point * sizeof(uint32_t));
-        //CUDA_MEMCPY(deviceTab, hostTab, nb_point*sizeof(uint32_t), cudaMemcpyHostToDevice);
-    }
 
-    //kernel_updateImage_GPU<<<nblocks, nthreads>>>(_zoom, _offsetX, _offsetY, IMAGE_WIDTH, IMAGE_HEIGHT, deviceTab, max_iters);
+    kernel_updateImage_GPU<<<grid, block>>>(_zoom, _offsetX, _offsetY, IMAGE_WIDTH, IMAGE_HEIGHT, deviceTab, max_iters);
 
     CUDA_MEMCPY(hostTab, deviceTab, nb_point*sizeof(uint32_t), cudaMemcpyDeviceToHost);
 
@@ -81,5 +89,5 @@ void Convergence_GPU::updateImage(const long double _zoom, const long double _of
             image.setPixel(x, y, colors->getColor(hostTab[x+y*IMAGE_WIDTH]));
         }
     }
-    printf("value (CPU) = %x \n", hostTab[50+200*IMAGE_WIDTH]);
+    printf("Stop GPU convergence\n");
 }
