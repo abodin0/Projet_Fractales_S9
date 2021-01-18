60bba45510dd403d35ee4905d3652af6c3bbc982
abodin0 <antton.bodin@live.com>
2021-01-18 11:43:02 +0100

    modif double openMP

--- src/Convergence/double_dp_omp_x86/Convergence_dp_omp_x86.cpp
+++ src/Convergence/double_dp_omp_x86/Convergence_dp_omp_x86.cpp
@@ -24,13 +24,13 @@ void Convergence_dp_omp_x86::updateImage(const long double _zoom, const long dou
     double offsetY = _offsetX;
     double zoom    = _zoom;
 
-    #pragma omp parallel for
+    #pragma omp parallel for num_threads(std::thread::hardware_concurrency()) schedule(dynamic)
 
     for (int y = 0; y < IMAGE_HEIGHT; y++) {
 
         double startImag = offsetY - IMAGE_HEIGHT / 2.0f * zoom + (y * zoom);
 
-        #pragma omp parallel for
+        #pragma omp parallel for num_threads(std::thread::hardware_concurrency()) schedule(dynamic)
 
         for (int x = 0; x < IMAGE_WIDTH;  x++) {
 
